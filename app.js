b882245ebc9a43acb639b47c7451e8941eb0c091
Shivam Rawat <shiv.rawat594@gmail.com>
2022-01-01 16:13:10 +0530

    changed cors origin

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['http://localhost:3000', 'https://nifty-elion-1e2c26.netlify.app'],
+        origin: ['http://localhost:3000', 'https://stoic-sammet-a92dfd.netlify.app'],
         credentials:true
     },
 ))
