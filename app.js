064428f00a36e545773029d0665884b6c3674c57
Shivam Rawat <shiv.rawat594@gmail.com>
2022-01-01 16:37:30 +0530

    same

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['http://localhost:3000', 'https://stoic-sammet-a92dfd.netlify.app'],
+        origin: ['http://localhost:3000', 'https://sleepy-kepler-c2f95b.netlify.app'],
         credentials:true
     },
 ))
