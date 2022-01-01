8a2d173754582155292fdd81b9437a93f08367e4
Shivam Rawat <shiv.rawat594@gmail.com>
2022-01-01 22:13:20 +0530

    changed

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['http://localhost:3000'],
+        origin: ['http://localhost:3000', 'https://agitated-swanson-e0827c.netlify.app'],
         credentials:true
     },
 ))
