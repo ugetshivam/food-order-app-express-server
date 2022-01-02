513f15244c5e2c97276a0a8e1dbafe61e53ad8de
Shivam Rawat <shiv.rawat594@gmail.com>
2022-01-02 16:51:57 +0530

    sign up

--- api/authRoutes.js
+++ api/authRoutes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
 
-router.get('/fakeuser', async (req, res)=>{
-    const user = new User({
-        username: 'sabeel',
-        email: 'sabeel@gmail.com'
-    });
+// router.get('/fakeuser', async (req, res)=>{
+//     const user = new User({
+//         username: 'sabeel',
+//         email: 'sabeel@gmail.com'
+//     });
 
-   const newUser = await User.register(user, 'sabeel12');
-   res.send(newUser);
+//    const newUser = await User.register(user, 'sabeel12');
+//    res.send(newUser);
+// })
+
+router.get('/register', (req,res)=>{
+    console.log(req.body);
 })
-
-
 
 module.exports = router;
